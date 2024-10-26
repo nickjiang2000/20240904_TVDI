63c263dba87d65afec48143d1ef77c79e844dfa5
Nick@Home <nickkiang@gmail.com>
2024-10-26 16:20:23 +0800

    Homework

--- Lesson5/lesson5_3.py
+++ Lesson5/lesson5_3.py
@@ -15,29 +15,19 @@ class Window(ThemedTk):
         #==============top Frame===============
 
         topFrame = ttk.Frame(self)
-        ttk.Label(topFrame,text='國破山河在，城春草木深。',style='TopFrame.TLabel').pack()
+        ttk.Label(topFrame,text='check_box多選鈕',style='TopFrame.TLabel').pack()
         topFrame.pack(padx=20,pady=20)
         
         #==============end topFrame===============
 
         #==============bottomFrame===============
         bottomFrame = ttk.Frame(self)
-        self.agreement = tk.StringVar()
         
-        ttk.Checkbutton(bottomFrame,
-                text='I agree',
-                command=self.agreement_changed,
-                variable=self.agreement,
-                onvalue='感時花濺淚，恨別鳥驚心。',
-                offvalue='峰火連三月，家書抵萬金。').pack()
         
         bottomFrame.pack(expand=True,fill='x',padx=20,pady=(0,20),ipadx=10,ipady=10)
         #==============end bottomFrame===============
 
-    def agreement_changed(self):
-        showinfo(
-            title='Result',
-            message=self.agreement.get())
+    
         
 
 def main():
