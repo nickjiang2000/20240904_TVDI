f0de1b71ded267c535e203fbbfe0fca7c87f47f4
江榮展 <nickkiang@gmail.com>
2024-11-05 09:54:16 +0800

    lesson8_1

---
+++ Lesson8/Script-1.sql
@@ -0,0 +1,13 @@
+CREATE TABLE records (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	sitename TEXT NOT NULL,
+	county TEXT,
+	aqi INTEGER,
+	status TEXT,
+	pm25 NUMERIC,
+	date TEXT,
+	lat NUMERIC,
+	lon NUMERIC,
+	UNIQUE
+(date, sitename)
+);
