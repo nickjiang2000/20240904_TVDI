fd54d82754bf65ee3c06920be85c4f8cf84b5b65
江榮展 <nickkiang@gmail.com>
2024-11-05 10:31:36 +0800

    lesson8_3

--- Lesson8/view/sitename_frame.py
+++ Lesson8/view/sitename_frame.py
@@ -1,4 +1,20 @@
 from tkinter import ttk 
 
 class SitenameFrame(ttk.Frame):
-    pass
+    '''
+    SitenameFrame主要是提供一個自訂的Frame,當使用者選取城市時
+    必需要建立對應的SitenameFrame。
+    SitenameFrame內會使用chechbox_widget,提供給使用者會勾選那一個站點
+    '''
+    def __init__(self,master=None,sitenames:list[str]=[],**kwargs):
+        super().__init__(master=master, **kwargs)
+        #欄寬度的權重
+        self.columnconfigure(0,weight=1)
+        self.columnconfigure(1,weight=1)
+        for idx,value in enumerate(sitenames):
+            column = idx % 2
+            index = int(idx / 2)
+            print(idx,value)
+            print(f'column:{column}')
+            print(f'index:{index}')
+            print("================")
